25e03f0bbd928c4f376169065f118884bda7706a
yuchenlin <yuchenl@allenai.org>
2023-12-21 05:37:44 +0000

    vllm update

--- src/vllm_infer/README.md
+++ src/vllm_infer/README.md
@@ -6,25 +6,72 @@ conda activate vllm
 pip install vllm
 pip install datasets
 
+```
+
+
+### Using vLLM APIs.
+```bash 
 # https://docs.vllm.ai/en/latest/models/engine_args.html
 
 python -m vllm.entrypoints.api_server \
         --download-dir /net/nfs/s2-research/llama2/ \
         --host 127.0.0.1 --port 2333 \
-        --model meta-llama/Llama-2-70b-hf --tensor-parallel-size 2 --dtype bfloat16 
+        --model meta-llama/Llama-2-70b-hf --tensor-parallel-size 4 --dtype bfloat16 
 
 python src/vllm_infer/vllm_urial.py \
     --data_name alpaca_eval \
+    --vllm_url http://localhost:2333/generate \
     --model_name Llama-2-70b-hf \
     --urial_name inst_help \
-    --top_p 1 --temperature 0 --repetition_penalty 1.1
+    --top_p 1 --temperature 0 --repetition_penalty 1.05
 
 
 python src/vllm_infer/vllm_urial.py \
     --data_name alpaca_eval \
+    --vllm_url http://localhost:2333/generate \
     --model_name Llama-2-70b-hf \
-    --urial_name inst_1k \
+    --urial_name inst_help \
     --top_p 0.9 --temperature 0.7 --repetition_penalty 1.1
+```
+
+
+### Using vLLM local inference 
+```bash
+python src/vllm_infer/vllm_urial.py \
+    --data_name alpaca_eval \
+    --vllm_mode local \
+    --download_dir /net/nfs/s2-research/llama2/ \
+    --model_name meta-llama/Llama-2-13b-hf --tensor_parallel_size 4  --dtype bfloat16 \
+    --urial_name inst_help \
+    --output_folder "vllm_outputs/alpaca_eval/" \
+    --top_p 0.9 --temperature 0.5 --repetition_penalty 1.0 --batch_size 32 --max_tokens 4096
+
+python src/vllm_infer/vllm_urial.py \
+    --data_name alpaca_eval \
+    --vllm_mode local \
+    --download_dir /net/nfs/s2-research/llama2/ \
+    --model_name mistralai/Mixtral-8x7B-v0.1 --tensor_parallel_size 4   --dtype bfloat16 \
+    --urial_name inst_help \
+    --output_folder "vllm_outputs/alpaca_eval/" \
+    --top_p 1 --temperature 0 --repetition_penalty 1.2 --batch_size 32 --max_tokens 4096
+
+    
+
+CUDA_VISIBLE_DEVICES=2,3 python src/vllm_infer/vllm_urial.py \
+    --data_name just_eval \
+    --vllm_mode local \
+    --download_dir /net/nfs/s2-research/llama2/ \
+    --model_name allenai/tulu-2-dpo-70b --tensor_parallel_size 2  --dtype bfloat16 \
+    --output_folder "vllm_outputs/just_eval/" \
+    --top_p 1 --temperature 0 --batch_size 800 --max_tokens 7500
+
+CUDA_VISIBLE_DEVICES=2,3 python src/vllm_infer/vllm_urial.py \
+    --data_name just_eval \
+    --vllm_mode local \
+    --download_dir /net/nfs/s2-research/llama2/ \
+    --model_name meta-llama/Llama-2-70b-chat-hf --tensor_parallel_size 2  --dtype bfloat16 \
+    --output_folder "vllm_outputs/just_eval/" \
+    --top_p 1 --temperature 0 --batch_size 800 --max_tokens 7500
 ```
 
 
@@ -37,10 +84,15 @@ python -m vllm.entrypoints.api_server \
         --host 127.0.0.1 --port 2333 \
         --model mistralai/Mixtral-8x7B-v0.1 --tensor-parallel-size 8
 
+CUDA_VISIBLE_DEVICES=2,3 python -m vllm.entrypoints.api_server \
+        --download-dir /net/nfs/s2-research/llama2/ \
+        --host 127.0.0.1 --port 2335 \
+        --model mistralai/Mistral-7B-v0.1 --tensor-parallel-size 2 --dtype bfloat16
+
 python src/vllm_infer/vllm_urial.py \
     --data_name alpaca_eval \
-    --vllm_url http://localhost:2333/generate \
-    --model_name Mixtral-8x7B-v0.1 \
-    --urial_name inst_help \
-    --top_p 1 --temperature 0
+    --vllm_url http://localhost:2335/generate \
+    --model_name Mistral-7B-v0.1 \
+    --urial_name inst_1k \
+    --top_p 0.9 --temperature 0.7 --repetition_penalty 1.1
 ```
--- src/vllm_infer/vllm_urial.py
+++ src/vllm_infer/vllm_urial.py
@@ -7,16 +7,25 @@ from tqdm import tqdm
 import json
 import os 
 
+
+
 def parse_args():
     parser = argparse.ArgumentParser() 
+    parser.add_argument('--vllm_mode', default="api", type=str) # api or local
     parser.add_argument('--vllm_url', default="http://localhost:2333/generate", type=str)
-    parser.add_argument('--model_name', default="Llama-2-70b-hf", type=str)
+    parser.add_argument('--output_folder', default="vllm_outputs", type=str)
+    parser.add_argument('--download_dir', default=None, type=str)
+    
+    parser.add_argument('--model_name', default="meta-llama/Llama-2-70b-hf", type=str)
+    parser.add_argument('--tensor_parallel_size', type=int, default=2)
+    parser.add_argument('--dtype', type=str, default="auto")
     parser.add_argument('--data_name', default="alpaca_eval", type=str) 
-    parser.add_argument('--urial_name', default="inst_help", type=str)
+    parser.add_argument('--urial_name', default="none", type=str)
+    parser.add_argument('--batch_size', default=1, type=int)
     parser.add_argument('--top_p',default=1, type=float)
     parser.add_argument('--temperature',default=0, type=float)
     parser.add_argument('--repetition_penalty',default=1, type=float)
-    
+    parser.add_argument('--max_tokens',default=7500, type=int)
     return parser.parse_args()
 
 def load_eval_data(data_name="alpaca_eval"):
@@ -28,6 +37,7 @@ def load_eval_data(data_name="alpaca_eval"):
         metadata = {"dataset": []}
     elif data_name == "just_eval":
         dataset = load_dataset("re-align/just-eval-instruct", split="test") 
+        metadata = {"dataset": [], "source_id": []}
     else:
         print("ERROR: data_name not supported")
             
@@ -46,12 +56,24 @@ def load_urial_prompt(split_name="inst_help"):
     return urial_prompt
 
 def apply_urial(pure_input_texts, urial_prompt):
-    urial_inputs = []
+    model_inputs = []
     for text_in in pure_input_texts:
         urial_in = f"{urial_prompt}\n# Query:\n```{text_in}```\n\n# Answer:\n```\n"
-        urial_inputs.append(urial_in)
-    return urial_inputs
+        model_inputs.append(urial_in)
+    return model_inputs
 
+
+def apply_template(pure_input_texts, model_name):
+    model_inputs = []
+    for instruction in pure_input_texts:
+        if "tulu-2" in model_name.lower():
+            prompt_url = "https://raw.githubusercontent.com/tatsu-lab/alpaca_eval/main/src/alpaca_eval/models_configs/tulu-2-dpo-70b/prompt.txt"
+        elif "llama" in model_name.lower() and "chat" in model_name.lower():
+            prompt_url = "https://raw.githubusercontent.com/tatsu-lab/alpaca_eval/main/src/alpaca_eval/models_configs/llama-2-7b-chat-hf/prompt.txt"
+        prompt = urllib.request.urlopen(prompt_url).read().decode('utf-8')
+        model_intput = prompt.replace("{instruction}", instruction)
+        model_inputs.append(model_intput)
+    return model_inputs
 
 def vllm_request(
     url='http://localhost:2333/generate',
@@ -105,39 +127,93 @@ def save_outputs(args, outputs, pure_input_texts, metadata, filepath):
         for ind in range(len(outputs)):
             output_item = {}
             output_item["instruction"] = pure_input_texts[ind]
-            output_item["output"] = outputs[ind][0].replace("```", " ").rstrip()
-            output_item["generator"] = args.model_name + "+URIAL=" + args.urial_name + f".p={args.top_p}.t={args.temperature}.r={args.repetition_penalty}"
+            if args.urial_name != "none":
+                output_item["output"] = outputs[ind][0].replace("```", " ").rstrip()
+                output_item["generator"] = args.model_name + "+URIAL=" + args.urial_name + f".p={args.top_p}.t={args.temperature}.r={args.repetition_penalty}"
+            else:
+                output_item["output"] = outputs[ind][0].rstrip()
+                output_item["generator"] = args.model_name + f".p={args.top_p}.t={args.temperature}"
             output_item["dataset"] = metadata["dataset"][ind]
             formatted_outputs.append(output_item)
-    
+    elif args.data_name == "just_eval":
+        for ind in range(len(outputs)):
+            output_item = {}
+            output_item["id"] = ind
+            output_item["instruction"] = pure_input_texts[ind]
+            if args.urial_name != "none":
+                output_item["output"] = outputs[ind][0].replace("```", " ").rstrip()
+                output_item["generator"] = args.model_name + "+URIAL=" + args.urial_name + f".p={args.top_p}.t={args.temperature}.r={args.repetition_penalty}"
+            else:
+                output_item["output"] = outputs[ind][0].rstrip()
+                output_item["generator"] = args.model_name + f".p={args.top_p}.t={args.temperature}"
+            output_item["dataset"] = metadata["dataset"][ind]
+            output_item["source_id"] = metadata["source_id"][ind]
+            output_item["datasplit"] = "just_eval"
+            formatted_outputs.append(output_item)
     with open(filepath, "w") as f:
         json.dump(formatted_outputs, f, indent=2)
-        
+
+
+def vllm_local_completions():
+    pass         
 
 if __name__ == "__main__":
     args = parse_args()
-    filepath = f"vllm_outputs/{args.model_name}.URIAL={args.urial_name}.p={args.top_p}.t={args.temperature}.r={args.repetition_penalty}.json" 
     
+    # Model loading 
+    if args.vllm_mode == "local":
+        from vllm import LLM, SamplingParams # when running locally 
+        llm = LLM(model=args.model_name, tokenizer=args.model_name, tokenizer_mode="auto", tensor_parallel_size=args.tensor_parallel_size, download_dir=args.download_dir, dtype=args.dtype)        
+        
+    # Decide the output filepath 
+    if "/" in args.model_name:
+        args.model_name = args.model_name.split("/")[1]   
+    
+    os.system(f"mkdir -p {args.output_folder}")
+    filepath = f"{args.output_folder}/{args.model_name}.URIAL={args.urial_name}.p={args.top_p}.t={args.temperature}.r={args.repetition_penalty}.json" 
+    
+    
+    # Data loading 
+       
     id_strs, pure_input_texts, metadata = load_eval_data(args.data_name)
-    urial_prompt = load_urial_prompt(args.urial_name)
-    urial_inputs = apply_urial(pure_input_texts, urial_prompt)
+    if args.urial_name != "none":
+        urial_prompt = load_urial_prompt(args.urial_name)
+        model_inputs = apply_urial(pure_input_texts, urial_prompt)
+    else:
+        model_inputs = apply_template(pure_input_texts, args.model_name)
 
-    # if the file exists, load it
-    
+    # if the file exists, load it and skip the processed examples
     outputs = []
     if os.path.exists(filepath):
         with open(filepath) as f:
             formatted_outputs = json.load(f)
         for output_item in formatted_outputs:
             outputs.append([output_item["output"]])
-    
     start_index = len(outputs)
     print(f"We skipped the first {start_index} examples")
-    for ind, prompt in tqdm(enumerate(tqdm(urial_inputs[start_index:]))):
-        output = vllm_request(url=args.vllm_url, prompt=prompt, n=1, stop=["# Query"], max_tokens=2048, top_p=args.top_p, temperature=args.temperature, repetition_penalty=args.repetition_penalty)
-        outputs.append(output)
-        # if len(outputs) % 2 == 0:
-        save_outputs(args, outputs, pure_input_texts, metadata, filepath)
+    
+    
+    stop_words = []
+    if args.urial_name != "none":
+        stop_words = ["# Query"]
+    
+    
+    # Run the inference 
+    if args.vllm_mode == "api":
+        for ind, prompt in tqdm(enumerate(tqdm(model_inputs[start_index:]))):
+            output = vllm_request(url=args.vllm_url, prompt=prompt, n=1, stop=stop_words, max_tokens=args.max_tokens, top_p=args.top_p, temperature=args.temperature, repetition_penalty=args.repetition_penalty)
+            outputs.append(output)
+            # if len(outputs) % 2 == 0:
+            save_outputs(args, outputs, pure_input_texts, metadata, filepath)
+    elif args.vllm_mode == "local":
+        sampling_params = SamplingParams(top_p=args.top_p, temperature=args.temperature, repetition_penalty=args.repetition_penalty, max_tokens=args.max_tokens, stop=stop_words)
+        todo_inputs = model_inputs[start_index:]
+        for cur_id in tqdm(range(0, len(todo_inputs), args.batch_size), desc="Batching"):
+            batch_inputs = todo_inputs[cur_id:cur_id+args.batch_size]
+            batch_outputs = llm.generate(batch_inputs, sampling_params)
+            outputs.extend([[x.outputs[0].text] for x in batch_outputs])
+            # print(outputs[-1])
+            save_outputs(args, outputs, pure_input_texts, metadata, filepath)
     save_outputs(args, outputs, pure_input_texts, metadata, filepath)
     # args.model_name = args.model_name.split("/")[1]
     
